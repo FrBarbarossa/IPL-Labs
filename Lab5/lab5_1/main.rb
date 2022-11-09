93bfea6a31c40f998eb075c99c37c2c75f7099c6
FrBarbarossa <91803857+FrBarbarossa@users.noreply.github.com>
2022-11-09 20:08:45 +0300

    Revert "Lab-5 approved"

--- Lab5/lab5_1/main.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require_relative 'some_cos'
-include SomeCos
-
-a = Integer(gets.chomp)
-b = Integer(gets.chomp)
-puts SomeCos.calculate(a, b)
--- Lab5/main.rb
+++
@@ -1,4 +0,0 @@
-require_relative 'array_sorter'
-
-arr = gets.chop.split(' ')
-puts ArraySorter.sort_by_length(arr)
