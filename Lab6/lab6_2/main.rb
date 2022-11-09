d855500aa859efe7a4dc81c586aa498c61499e09
FrBarbarossa <91803857+FrBarbarossa@users.noreply.github.com>
2022-11-09 19:51:40 +0300

    Missmerge

--- Lab6/lab6_2/main.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require_relative 'accuracy'
-
-1.upto(10) do |i|
-  result = Accuracy.new.find { |prev, sum| (prev - sum).abs < 10**-i }
-  puts "При точности #{i} знак(ов) после запятой значение = #{result[1]}, число интераций: #{result[2].to_int}"
-end
